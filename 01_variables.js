b46468d97bb3c3ef33921f64f5ec0766d931bce8
ayushash1 <104675298+ayushash1@users.noreply.github.com>
2023-08-03 15:35:47 +0530

    added notes

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+// variable is a container 
+// very simillar to container used to store things at home
+
+let a = 67 // a contains 67
+console.log(a)
+
+a = "ayush" // changed datatype for same variable "a"
+console.log(a)
+
+//// declaring variables
+// two ways to declare variables
+let b = 7; 
+var c = 7; 
+// letters digits, undercases and $ are allowed
+// cant assingn variable starting number, reserved keywords
+// must begin with a $, _ or a letter
+// javascript is case sensitive
