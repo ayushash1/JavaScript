292687c32303a07c24082550a0e5623668de47e2
ayushash1 <104675298+ayushash1@users.noreply.github.com>
2023-08-04 18:45:02 +0530

    operators

---
+++ 05_operators_and_expressions.js
@@ -0,0 +1,31 @@
+// Expression
+// a fragment of code that produces a value is called an expression 
+// every value written littrelly is an expression for ex: 77 or "ayush"
+
+// Operators in JavaScript
+// 01. Arithimate Operators
+// +, -,
+
+let a = 45;
+let b = 7;
+console.log("Arithimatic Operators")
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a * b = ", a*b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+console.log("a++ =", a++)
+console.log("++a =", ++a)
+console.log("a-- =", a--)
+console.log("--a =", --a)
+
+// 02. Assignment Operators
+// =, +=, -=, *=, /=, %=, **=
+
+console.log("assingment operator")
+let assingnment = 1;
+assingnment += 5;
+console.log(assingnment)
+
+// 03. Comaparism Operator
