b808fa075482e4ff47226cef814c3fe0e518db5c
ayushash1 <104675298+ayushash1@users.noreply.github.com>
2023-08-08 18:02:48 +0530

    practice set

--- 07_practice_set2.js
+++ 07_practice_set2.js
@@ -1,26 +1,46 @@
 const prompt = require('prompt-sync')();
-// // Q01;
-let age = prompt("what is your age");
-if(age>10 && age<20){
-    console.log("Your age is between ")
-}
-else{
-    console.log("your age does not ly between 10 and 12")
-}
+// Q01;
+// let age = prompt("what is your age");
+// if(age>10 && age<20){
+//     console.log("Your age is between ")
+// }
+// else{
+//     console.log("your age does not ly between 10 and 12")
+// }
 
-// Q02;
-let age2 = prompt("What is your age");
-switch(age2){
-    case 12:
-        console.log("your age is 12")
+// // Q02;
+// let age2 = prompt("What is your age");
+// switch(age2){
+//     case 12:
+//         console.log("your age is 12")
+//         break
     
-    case 13:
-        console.log("your age is 13")
+//     case 13:
+//         console.log("your age is 13")
+//         break
 
-    case 14:
-        console.log("your age is 14")
+//     case 14:
+//         console.log("your age is 14")
+//         break
     
-    default:
-        console.log("your age is not speacial")
+//     default:
+//         console.log("your age is not speacial")
 
-}
+// }
+
+// Q03
+
+// let num = prompt("What is your number? ")
+// num = Number.parseInt(num)
+// if(num%2==0 && num%3==0){
+//     console.log("your number is divisible by 2 and 3")
+// }
+// else{
+//     console.log("Your number is not divisible by 2 and 3")
+// }
+
+// Q04 
+// let age = 18
+// let a = age>=18 ? "you can drive": "you cannot drive"
+// console.log(a)
+
